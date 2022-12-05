b1aa038cdb009af72aa7ae07b8d368249cf9a2d4
shridharrai-nickelfox <shridhar.rai@nickelfox.com>
2022-12-05 17:38:30 +0530

    Backend

---
+++ middleware/requireAuth.js
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/userModel");
+
+// verify user is authenticated
+const requireAuth = async (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization)
+    return res.status(401).json({ error: "Authorization token required" });
+
+  const token = authorization.split(" ")[1];
+
+  try {
+    const { _id } = jwt.verify(token, process.env.SECRET);
+    req.user = await User.findOne({ _id }).select("_id");
+    next();
+  } catch (error) {
+    console.log(error);
+    res.status(401).json({ error: "Request is not Authorized" });
+  }
+};
+
+module.exports = requireAuth;
